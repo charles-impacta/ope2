437147d0e861e4a80494267e1f42811f839b55ea
Charles Kilesse <charles.kilesse@aluno.faculdadeimpacta.com.br>
2020-09-19 17:19:05 -0300

    adiciona models Produto e Categoria

--- backend_api/app/model.py
+++ backend_api/app/model.py
@@ -2,16 +2,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class Usuario(db.Model):
     __tablename__ = 'usuarios'
 
     id = db.Column(db.Integer, primary_key=True)
     login = db.Column(db.String(32), unique=True, index=True)
     senha = db.Column(db.String(64))
+
     estabelecimento_id = db.Column(db.Integer, db.ForeignKey('estabelecimentos.id'))
 
     def __repr__(self):
         return '<Usuario %r>' % self.login
+
 
 class Estabelecimento(db.Model):
     __tablename__ = 'estabelecimentos'
@@ -19,7 +22,38 @@ class Estabelecimento(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(64), unique=True, index=True)
     cnpj = db.Column(db.String(14), unique=True)
+
     usuarios = db.relationship('Usuario', backref='estabelecimento')
+    produtos = db.relationship('Produto', backref='estabelecimento')
 
     def __repr__(self):
         return '<Estabelecimento %r>' % self.nome
+
+
+class Produto(db.Model):
+    __tablename__ = 'produtos'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(64), unique=True, index=True)
+    descricao = db.Column(db.String(256))
+    ingredientes = db.Column(db.String(512))
+    modo_de_preparo = db.Column(db.String(512))
+    preco = db.Column(db.Float)
+
+    estabelecimento_id = db.Column(db.Integer, db.ForeignKey('estabelecimentos.id'))
+    categoria_id = db.Column(db.Integer, db.ForeignKey('categorias.id'))
+
+    def __repr__(self):
+        return '<Produto %r>' % self.nome
+
+
+class Categoria(db.Model):
+    __tablename__ = 'categorias'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(64), unique=True, index=True)
+
+    produtos = db.relationship('Produto', backref='categoria')
+
+    def __repr__(self):
+        return '<Categoria %r>' % self.nome
