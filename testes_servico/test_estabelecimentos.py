818652866bc0a93becec5e8d1ac2706cb9b80bbb
Charles Kilesse <charles.kilesse@aluno.faculdadeimpacta.com.br>
2020-09-28 10:31:49 -0300

    adiciona testes estabelecimentos

--- testes_servico/test_estabelecimentos.py
+++ testes_servico/test_estabelecimentos.py
@@ -3,7 +3,7 @@ import string
 import random
 import requests
 
-from .base_test import BaseTest
+from base_test import BaseTest
 
 
 class TestEstabelecimentos(BaseTest):
@@ -41,6 +41,88 @@ class TestEstabelecimentos(BaseTest):
         assert r_json['id'] > 0
         assert r_json['mensagem'] == 'Estabelecimento criado com sucesso.'
 
+    def test_atualiza_estabelecimento(self):
+        # arrange
+        data = self._test_data()
+        id_estabelecimento = self._post_estabelecimentos(data).json()['id']
+
+        put_data = self._test_data()
+
+        # act
+        r = requests.put(self._url + str(id_estabelecimento), json=put_data)
+        r_json = r.json()
+
+        # assert
+        assert r.ok
+        assert r.status_code == 200
+
+        assert len(r_json) == 3
+        assert r_json['id'] > 0
+        assert r_json['cnpj'] == put_data['cnpj']
+        assert r_json['nome'] == put_data['nome']
+
+    def test_deleta_estabelecimento(self):
+        # arrange
+        data = self._test_data()
+        id_estabelecimento = self._post_estabelecimentos(data).json()['id']
+
+        # act
+        r = requests.delete(self._url + str(id_estabelecimento))
+        r_json = r.json()
+
+        # assert
+        assert r.ok
+        assert r.status_code == 200
+
+        assert len(r_json) == 1
+        assert r_json['mensagem'] == 'Estabelecimento deletado com sucesso.'
+
+    def test_busca_estabelecimento_por_id(self):
+        # arrange
+        data = self._test_data()
+        id_estabelecimento = self._post_estabelecimentos(data).json()['id']
+
+        # act
+        r = requests.get(self._url + str(id_estabelecimento))
+        r_json = r.json()
+
+        # assert
+        assert r.ok
+        assert r.status_code == 200
+
+        assert len(r_json) == 3
+        assert r_json['id'] > 0
+        assert r_json['cnpj'] == data['cnpj']
+        assert r_json['nome'] == data['nome']
+
+    def test_busca_todos_estabelecimentos(self):
+        # arrange
+        data_1 = self._test_data()
+        id_estabelecimento_1 = self._post_estabelecimentos(data_1).json()['id']
+        data_2 = self._test_data()
+        id_estabelecimento_2 = self._post_estabelecimentos(data_2).json()['id']
+
+        # act
+        r = requests.get(self._url)
+        r_json = r.json()
+
+        estabelecimento_1 = self._busca_etabelecimento_na_lista(r_json, id_estabelecimento_1)
+        estabelecimento_2 = self._busca_etabelecimento_na_lista(r_json, id_estabelecimento_2)
+
+        # assert
+        assert r.ok
+        assert r.status_code == 200
+
+        assert len(estabelecimento_1) == 3
+        assert estabelecimento_1['id'] > 0
+        assert estabelecimento_1['cnpj'] == data_1['cnpj']
+        assert estabelecimento_1['nome'] == data_1['nome']
+
+        assert len(estabelecimento_2) == 3
+        assert estabelecimento_2['id'] > 0
+        assert estabelecimento_2['cnpj'] == data_2['cnpj']
+        assert estabelecimento_2['nome'] == data_2['nome']
+
 
     #
     # metodos de suporte
@@ -59,3 +141,10 @@ class TestEstabelecimentos(BaseTest):
 
     def _delete_estabelecimentos(self, id):
         requests.delete(self._url + str(id))
+
+    def _busca_etabelecimento_na_lista(self, lista, id):
+        result = None
+        for estabelecimento in lista:
+            if estabelecimento['id'] == id:
+                result = estabelecimento
+        return result
