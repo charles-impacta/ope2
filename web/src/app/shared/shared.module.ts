ed487cfe7ba2c842f6c46968729c63a57677280d
fake-user <laercio.bsilva@gmail.com>
2020-10-06 17:55:36 -0300

    Ajuste de Busca de Produto

--- web/src/app/shared/shared.module.ts
+++ web/src/app/shared/shared.module.ts
@@ -2,14 +2,15 @@ import { StatusFlagComponent } from './status-flag/status-flag.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AppRoutingModule } from '../app-routing.module';
+
+import { FoodNotFoundComponent } from './food-not-found/food-not-found.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ NavBarComponent,StatusFlagComponent],
-  exports: [ NavBarComponent,StatusFlagComponent]
+  declarations: [ NavBarComponent,StatusFlagComponent, FoodNotFoundComponent],
+  exports: [ NavBarComponent,StatusFlagComponent,FoodNotFoundComponent]
 })
 export class SharedModule {
 
