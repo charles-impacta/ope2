85181a7c727de33ca5e525059388c5bff10c390b
Charles Kilesse <charles.kilesse@aluno.faculdadeimpacta.com.br>
2020-10-24 11:46:10 -0300

    atualiza host backend

--- web/src/app/shared/constants.ts
+++ web/src/app/shared/constants.ts
@@ -1,5 +1,5 @@
 export class Constants {
 
-  public static hostApi : string = window.location.host.indexOf('localhost') >= 0 ? 'http://localhost:5000' : 'http://impactados2020.live:5000';
+  public static hostApi : string = window.location.host.indexOf('localhost') >= 0 ? 'http://localhost:5000' : 'http://api.impactados2020.live:5000';
 
 }
