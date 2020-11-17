ccc07558516ce793f2868af3526edf0b9faf37c5
Laercio <laercio.bsilva@gmail.com>
2020-11-17 14:46:27 -0300

    Ajustes Finais

--- web/src/app/default/default.component.ts
+++ web/src/app/default/default.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
 import { EstabelecimentoService } from 'src/domain/services/estabelecimento.service';
+import { AppBaseComponent } from '../shared/app-base-component';
 
 @Component({
   selector: 'app-default',
   templateUrl: './default.component.html',
   styleUrls: ['./default.component.scss']
 })
-export class DefaultComponent implements  OnInit {
+export class DefaultComponent extends AppBaseComponent implements  OnInit {
 
   title = 'Cardápio na Web';
   hasEstabelecimento : boolean = false;
-  constructor(private estabelecimentoService : EstabelecimentoService) {
+  constructor(injector: Injector,private estabelecimentoService : EstabelecimentoService) {
+
+    super(injector);
     this.estabelecimentoService.hasEstabelecimento().subscribe((result)=>{
       this.hasEstabelecimento = result;
     });
